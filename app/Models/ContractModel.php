919c2297b9db370ed6f2e8357a58e34dbc525efd
datalab4me <39529245+datalab4me@users.noreply.github.com>
2024-02-08 21:17:41 -0300

    Modulo para editar contrato padrao

---
+++ app/Models/ContractModel.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use App\Models\Referral;
+use App\Models\Package;
+use App\Models\Observation;
+use App\Models\Estimate;
+class ContractModel extends Model
+{
+    protected $table = 'contract_models';
+}
